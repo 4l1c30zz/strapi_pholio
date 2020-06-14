d8dca33ce318f2e8ddd0c4e362e3d8c3e9ff4799
4l1c30zz <4l1c30zz@gmail.com>
2020-06-14 09:01:12 +0300

    sticky added

--- frontend/src/components/header.vue
+++ frontend/src/components/header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header>
+  <header id="site-header">
 <div class="wrap boxed">
   <svg id="morph"  width="100" viewBox="0 300 1920 800" preserveAspectRatio="none">
          <path class="morph" d="M1-1C86.5,1.8,222.9,5.9,391,9c297.1,5.4,531.2,4.6,628,4c171.8-1,187.1-2.9,572-8c165.7-2.2,301.4-3.7,390.7-4.7C1321.4-0.1,661.2-0.6,1-1z"></path>
@@ -29,10 +29,11 @@
         </router-link>
         </li>
 
-        <li>
+        <li class="mark_block_wrap">
         <router-link
           to="/blog"
           v-slot="{ href, route, navigate }"
+          class="word"
         >
           <a  :href="href" @click="navigate"
             >{{ route.name }}</a
@@ -78,32 +79,72 @@ export default {
     `
   }
 };
+
+function stick (){
+  let head = document.getElementById("site-header");
+  let elem_height = head.offsetHeight;
+  if( window.pageYOffset >= elem_height ) {
+      head.classList.add("a");
+    } else{
+      head.classList.remove("a");
+    }
+}
+window.onscroll =  function() {
+  stick()
+};
 </script>
 <style lang="scss">
 @import "@/scss/_variables.scss";
 @import "@/scss/_functions.scss";
 @import "@/scss/_mixins.scss";
-header .wrap.boxed{
-  width: 100%;
-}
-.logo {
-    flex-basis: 30%;
-    text-align: left;
-    svg {
-        max-width: 90px;
-        margin: 0 auto;
+header {
+    position: static;
+    background: white;
+    transition: all 0.3 ease;
+    .wrap.boxed {
+        width: 100%;
     }
-    path {
-        fill: black;
-        stroke: black;
+    .logo {
+        flex-basis: 30%;
+        text-align: left;
+        svg {
+            max-width: 90px;
+            margin: 0 auto;
+            transition: all 0.3s ease;
+        }
+        path {
+            fill: color(_black);
+            stroke: color(_black);
+        }
+        a {
+            display: block;
+            text-align: left;
+        }
+    }
+    .nav {
+        flex-basis: 70%;
+        @extend %flex-center-end;
+        li {
+            margin: 10px;
+        }
+    }
+
+    &.a {
+        position: fixed;
+        top: 0;
+        width: 100%;
+        z-index: 10;
+        background: magenta;
+        .logo svg {
+            max-width: 70px;
+        }
     }
 }
-.nav {
-    flex-basis: 70%;
-    @extend %flex-center-end;
-    li{
-    margin: 10px;
-    }
+#morph {
+    pointer-events: none;
+        position: absolute;
+
 }
+
 
 </style>
