aa12846e55261f545264ffad152f0d0ed606245e
4l1c30zz <4l1c30zz@gmail.com>
2020-06-15 12:37:51 +0300

    oevrlay added

--- frontend/src/components/gallerySingle.vue
+++ frontend/src/components/gallerySingle.vue
@@ -1,18 +1,21 @@
 <template>
-<div>
-  <div class="gallery wrap gal">
-    <div
-    v-for="item in items" :key="item.url"
-    @click="class_toggle"
-    class="col-3 col-center i"
-    >
-        <img :src="api_url + item.url" class="gallery_image"  />
-        <h4 class="heading">{{item.caption}}</h4>
+
+<div class="gallery wrap gal">
+  <div
+  v-for="item in items" :key="item.url"
+  @click="class_toggle"
+  class="col-3 col-center i"
+  >
+    <img :src="api_url + item.url" class="gallery_image"  />
+    <h4 class="heading">{{item.caption}}</h4>
+  </div>
+  <div class="overlay">
+    <span class="close">close</span>
     </div>
-  </div>
-  <div class="overlay"></div>
-  </div>
+</div>
+
 </template>
+
 
 <script>
 
@@ -26,6 +29,9 @@ export default {
    class_toggle: function (event) {
      let targ = event.target.closest('.i');
      targ.classList.toggle('a');
+    let targ_parent = event.target.parentNode.closest('.gal');
+    console.log(targ_parent);
+    targ_parent.classList.add("a");
      let siblings = get_siblings(targ);
      siblings.forEach((sib) => {
        sib.classList.remove('a');
@@ -70,6 +76,50 @@ let get_siblings = function(e){
   .gallery{
     width: 100%;
     overflow: hidden;
+     .overlay{
+        background: transparent;
+        width: 0;
+        height: 0;
+        overflow: hidden;
+        position: static;
+        z-index: 0;
+        transition: all 0.5s ease;
+          .close{
+            position: static;
+            width: 0;
+            height: 0;
+            overflow: hidden;
+            padding: 0;
+            font-size: 0;
+            transition: $trans-default;
+          }
+      }
+    &.a{
+      .overlay{
+        background: white;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 50;
+         .close{
+          background: color(_yellow);
+          padding: $inner_padd;
+          font-weight: font_weight(bolder);
+          font-family: font(lined_font);
+          text-align: center;
+          font-size: font_size(f18);
+          width: auto;
+          height: auto;
+          cursor: pointer;
+          position: absolute;
+          top: 5%;
+          right: 0;
+         }
+      }
+    }
     h4{
     background: color(_black);
     color: white;
@@ -83,6 +133,7 @@ let get_siblings = function(e){
       position: relative;
       background: transparent;
       transition: all 0.3s cubic-bezier(0.895, 0.030, 0.685, 0.220);
+
       img{
       width: 100%;
       max-height: 30vw;
@@ -92,6 +143,7 @@ let get_siblings = function(e){
       object-position: center top;
       transition:  all 0.3s cubic-bezier(0.895, 0.030, 0.685, 0.220);
       }
+
       &.a{
       position: fixed;
       top: 0;
@@ -99,22 +151,8 @@ let get_siblings = function(e){
       margin: 0;
       padding: 0;
       width: 100%;
-      z-index: 99;
-      background: white;
       height: 100vh;
-      &::after{
-        content: "close";
-        background: color(_yellow);
-        padding: $inner_padd;
-        font-weight: font_weight(bolder);
-        font-family: font(lined_font);
-        text-align: center;
-        font-size: font_size(f18);
-        cursor: pointer;
-        position: absolute;
-        top: 5%;
-        right: 0;
-      }
+      z-index: 99;
         img{
         height: 100%;
         max-height: 90vh;
